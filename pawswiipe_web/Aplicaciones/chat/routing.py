de4e92dc5b69ffaf1804e6a196962e9434983a4b
Sergioo-D <sergio.dominguez.garcia@ieselcalamot.com>
2024-03-11 16:52:32 +0100

    prueba de chats

--- pawswiipe_web/Aplicaciones/chat/routing.py
+++ pawswiipe_web/Aplicaciones/chat/routing.py
@@ -1,6 +1,15 @@
 from django.urls import re_path
 from . import consumers
 
-websocket_urlpatterns = [
+""" websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<user1_id>\w+)/(?P<user2_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
+] """
+
+
+from django.urls import path
+from Aplicaciones.bbdd import views
+
+urlpatterns = [
+    path('direct/inbox/', views.inbox, name='inbox'),
+    path('direct/chat/<int:user1_id>/<int:user2_id>/', views.chat, name='chat'),
 ]
